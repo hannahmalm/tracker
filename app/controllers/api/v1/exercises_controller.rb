cf32e7ee22a78ae44d0948feb311e860c343b53b
Hannah Malm <hannahemalm616@gmail.com>
2022-01-11 16:55:27 -0500

    added create method in exercise controller

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -6,4 +6,19 @@ class Api::V1::ExercisesController < ApplicationController
         exercises = Exercise.all 
         render json: exercises
     end 
+
+    def create 
+        exercise = Exercise.new(exercise_params)
+        if exercse.save 
+            render json: exercise, status: :accepted #allows for satuts codes to be sent in the fetch
+        else 
+            render json: {errors: exercise.errors.full_messages}, status: :unprocessible_entity #422 error - usually caused because validations fail
+        end 
+    end
+
+
+    private
+        def exercise_params #exercise strong params - these are the only attributes that we will permit to be added into the database. Protects agains sql injection
+            params.require(:exercise).permit(:name, :instructions, :image, :category_id)
+        end 
 end
