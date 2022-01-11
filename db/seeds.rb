2a7a137c2769a137a37bed6bb7406079ad506f03
Hannah Malm <hannahemalm616@gmail.com>
2022-01-11 16:05:08 -0500

    added in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Exercise.destroy_all
+
+
+push = Category.create(title: "Push")
+pull= Category.create(title: "Pull")
+legs= Category.create(title: "Legs")
+
+
+deadlift = Exercise.create(name:"Deadlift",instructions:"Stand tall with your feet shoulder-width apart, holding a barbell off the floor with an overhand grip just outside your thighs. Keeping a slight bend in your knees, bend forwards from the hips – not the waist – and lower the bar down the front of your shins until you feel a good stretch in your hamstrings", image:"image_url", category_id: legs.id)
