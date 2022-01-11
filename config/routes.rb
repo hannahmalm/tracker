571e1c44ebae33286979b246555265dbc5b5e19e
Hannah Malm <hannahemalm616@gmail.com>
2022-01-11 16:34:34 -0500

    created exercise index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  #creating the namespace - run rails routes to verify that the route is correct
+  namespace :api do 
+    namespace :v1 do 
+      resouces :exercises, only: [:index]
+    end 
+  end 
+
 end
