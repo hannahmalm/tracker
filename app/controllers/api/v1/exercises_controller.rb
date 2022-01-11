00a41fd5bd89dda01619e032c80d3eee72408e0b
Hannah Malm <hannahemalm616@gmail.com>
2022-01-11 16:46:09 -0500

    added notes on fetch

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::ExercisesController < ApplicationController
 
     def index
         #used to render all the exercises
-        @exercises = Exercise.all 
-        render json: @exercises
+        #no longer rendering views - making a fetch request from the front and return all the json from the backend
+        exercises = Exercise.all 
+        render json: exercises
     end 
 end
